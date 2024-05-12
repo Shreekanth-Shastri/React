d8b7e6fb5eb2e021590be23ac20f185d953138cf
Shreekanth-Shastri <shreekanth.shastri@gmail.com>
2024-05-12 14:58:37 -0400

    react js

--- sample-project/src/App.js
+++ sample-project/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import PropTypes from "prop-types";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Job employeeName="Shreekanth" employerName="Self Employed" salary={123} />
   );
 }
+
+Job.propTypes = {
+  employeeName: PropTypes.string.isRequired,
+  employerName: PropTypes.string.isRequired,
+  salary: PropTypes.number
+};
+
+const Job = (props) => {
+  return (
+    <>
+      <div>{props.employeeName}</div>
+      <div>{props.employerName}</div>
+      <div>{props.salary}</div>
+    </>
+  );
+};
 
 export default App;
